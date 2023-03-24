7a00849c596e150441eec44aff183fa7a10ee211
RudranshPandey <99203088+RudranshPandey@users.noreply.github.com>
2023-03-24 22:51:59 +0530

    Create Kitchen Timings.cpp

---
+++ Kitchen Timings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,diff;
+	    cin>>a>>b;
+	    diff=b-a;
+	    cout<<diff<<endl;
+	}
+	return 0;
+}
